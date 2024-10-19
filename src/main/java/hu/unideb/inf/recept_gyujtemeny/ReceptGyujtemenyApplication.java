13d8e008ddfb134ad369334d2cfcf8b37c6bf9af
ofanni <ofanni03@gmail.com>
2024-10-19 14:42:45 +0200

    project init

---
+++ src/main/java/hu/unideb/inf/recept_gyujtemeny/ReceptGyujtemenyApplication.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.recept_gyujtemeny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceptGyujtemenyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceptGyujtemenyApplication.class, args);
+	}
+
+}
